6449cb69c93adab958ed9b24f5546fc096ac0bed
ARUNA DEVI S <140615648+S-ArunaDevi06@users.noreply.github.com>
2024-11-11 13:07:46 +0530

    Update by aruna.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-// src/App.js
-import React from "react";
+import React , {useRef}from "react";
 import Hero from "./components/Hero";
 import Events from "./components/Events";
-//import Timeline from "./components/Timeline";
+import Timeline from "./components/Timeline";
 import Statistics from "./components/Statistics";
 import Faculty from "./components/Faculty";
 import Committee from "./components/Committee";
 import Gallery from "./components/Gallery";
-import Contact from "./components/Contact"; 
-import Achievements from "./components/Achievements"; 
-import Credits from "./components/Credits";// Import the new component
+import Contact from "./components/Contact";
+import Speakers from "./components/Speakers.js"
 import './App.css';
 
 function App() {
+  const homeRef = useRef(null);
+  const eventsRef = useRef(null);
+  const speakersRef = useRef(null);
+  const committeeRef = useRef(null);
+  const contactRef = useRef(null);
+
+  
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="App">
-      <Hero />
+      <Hero 
+      homeRef={homeRef}
+      eventsRef={eventsRef}
+      speakersRef={speakersRef}
+      committeeRef={committeeRef}
+      contactRef={contactRef}
+      scrollToSection={scrollToSection} 
+      />
       <Events />
-      
+      <div ref={eventsRef}>
+        <Timeline />
+      </div>
       <Statistics />
-      <Faculty />
+      <div ref={speakersRef}>
+        <Speakers />
+      </div>
+      <div ref={committeeRef}>
+        <Faculty />
+      </div>
       <Committee />
       <Gallery />
-      <Achievements />
-      <Credits/>
-      <Contact />
-
+      <div ref={contactRef}>
+        <Contact />
+      </div>
     </div>
   );
 }
