2dba4bc26a35d4ae66f392c620d2ec7697b8650b
ARUNA DEVI S <140615648+S-ArunaDevi06@users.noreply.github.com>
2024-11-11 13:47:55 +0530

    Update Hero by aruna.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,34 @@
-// src/components/Hero.js
-import React from 'react';
+import logo from '../landing_image/logo.svg';
 import './Hero.css';
+import profile from '../landing_image/profile.jpeg';
+import React, { useRef } from 'react';
+import entrepreneur from '../landing_image/image.png';
 
-const Hero = () => {
-    return (
-        <div className="hero">
-            <div className="hero-overlay">
-                <h1 className="hero-title fade-in">Welcome to EDC</h1>
-                <p className="hero-text fade-in">Empowering Entrepreneurs of Tomorrow</p>
-            </div>
-        </div>
-    );
-};
 
-export default Hero;
+function App({ homeRef,eventsRef, speakersRef, committeeRef, contactRef, scrollToSection }){
+  
+
+  return (
+    <div className='Background'>
+      
+      <nav className='Header'>
+        <img src={logo} className="appLogo" alt="ReactLogo"></img>
+        <ul className='options'>
+          <listItem className='optionItem' onClick={() => scrollToSection(homeRef)} style={{ cursor: 'pointer' }}>Home</listItem>
+          <listItem className='optionItem' onClick={() => scrollToSection(eventsRef)} style={{ cursor: 'pointer' }}>Events</listItem>
+          <listItem className='optionItem' onClick={() => scrollToSection(speakersRef)} style={{ cursor: 'pointer' }}>Speakers</listItem>
+          <listItem className='optionItem' onClick={() => scrollToSection(committeeRef)} style={{ cursor: 'pointer' }}>Commitee</listItem>
+          <listItem className='optionItem' onClick={() => scrollToSection(contactRef)} style={{ cursor: 'pointer' }}>Contact</listItem>
+        </ul>
+        <img src={profile} className="profile" alt="profile"  style={{ cursor: 'pointer' }}></img>
+      </nav>
+
+      <div ref={homeRef} className='picture-description-container'>
+        <img src={entrepreneur} className='Entrepreneur-pic' alt="entreprenuer-image"></img>
+        <p className='Description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
